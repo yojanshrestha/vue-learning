067a544f456bbc92a998fff28e0b1f0a53311f0d
Yojan Shrestha <yojan.shrestha5023@gmail.com>
2023-05-12 15:46:54 +0545

    EP10

--- js/components/assignmentList.js
+++ js/components/assignmentList.js
@@ -4,18 +4,38 @@ export default {
         'assignment':  Assignment
     },
     'template': `
-                <section v-show="assignments.length">
-                    <h2>{{title}}</h2>
+                <section v-show="filteredAssignments.length">
+                    <h2>{{title}} <span>({{filteredAssignments.length}})</span></h2>
             
+                    <div>
+                        <button v-for="tag in tags"
+                         @click="currentTag = tag" 
+                         class="btn-normal"
+                         :class="{'btn-blue' : currentTag == tag }"
+                         >{{tag}}</button>
+                    </div>
                     <ul>
-                        <assignment v-for="assignment in assignments"
+                        <assignment v-for="assignment in filteredAssignments"
                             :assignment="assignment" :key="assignment.id"/>
                     </ul>
                 </section>`,
 
+    data(){
+      return {
+          currentTag: 'all'
+      }
+    },
     props:{
         assignments: Array,
-        title: String
+        title: String,
+    },
+    computed:{
+        tags(){
+            return ['all', ...new Set(this.assignments.map(a=>a.tag)) ]
+        },
+        filteredAssignments(){
+            return this.currentTag =='all' ? this.assignments : this.assignments.filter( a=>a.tag === this.currentTag)
+        }
     }
 
 }
--- js/components/assignments.js
+++ js/components/assignments.js
@@ -13,9 +13,9 @@ export default {
     data() {
         return {
             assignments: [
-                {name: 'Finish Project', complete: false, id: 1},
-                {name: 'Read Chapter 4', complete: false, id: 2},
-                {name: 'Turn in homework', complete: false, id: 3}
+                {name: 'Finish Project', complete: false, id: 1, tag:'math'},
+                {name: 'Read Chapter 4', complete: false, id: 2, tag:'science'},
+                {name: 'Turn in homework', complete: false, id: 3, tag:'math'}
             ],
         }
     },
